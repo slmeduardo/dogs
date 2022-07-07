209e89e52c66dd2c4ecfea815efcc8aadff42197
slmeduardo <itsmeeduardoc@gmail.com>
2022-07-07 10:39:24 -0300

    skeleton added

--- src/Components/Photo/PhotoContent.js
+++ src/Components/Photo/PhotoContent.js
@@ -1,5 +1,6 @@
 import { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import Image from '../../Helper/Image'
 import { UserContext } from '../../UserContext'
 import PhotoComments from './PhotoComments'
 import styles from './PhotoContent.module.css'
@@ -12,7 +13,7 @@ const PhotoContent = ({ data }) => {
   return (
     <div className={styles.photo}>
       <div className={styles.img}>
-        <img src={photo.src} alt={photo.title} />
+        <Image src={photo.src} alt={photo.title} />
       </div>
       <div className={styles.details}>
         <div>
--- src/Helper/Image.js
+++ src/Helper/Image.js
@@ -1,10 +1,24 @@
+import { useState } from 'react'
 import styles from './Image.module.css'
 
 const Image = ({ alt, ...props }) => {
+  const [skeleton, setSkeleton] = useState(true)
+
+  function handleLoad({ target }) {
+    setSkeleton(false)
+    target.style.opacity = 1
+  }
+
   return (
     <div className={styles.wrapper}>
-      <div className={styles.skeleton}></div>
-      <img className={styles.img} src="" alt={alt} {...props} />
+      {skeleton && <div className={styles.skeleton}></div>}
+      <img
+        onLoad={handleLoad}
+        className={styles.img}
+        src=""
+        alt={alt}
+        {...props}
+      />
     </div>
   )
 }
