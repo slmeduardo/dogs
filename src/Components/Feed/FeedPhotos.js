bf87cf921a48c4803fa12f15fce490c1f9c468f8
slmeduardo <itsmeeduardoc@gmail.com>
2022-07-07 13:22:19 -0300

    loading animation added

--- src/Components/Feed/FeedPhotos.js
+++ src/Components/Feed/FeedPhotos.js
@@ -6,17 +6,17 @@ import useFetch from '../../Hooks/useFetch'
 import styles from './FeedPhotos.module.css'
 import FeedPhotosItem from './FeedPhotosItem'
 
-const FeedPhotos = ({ setModalPhoto }) => {
+const FeedPhotos = ({ user, setModalPhoto }) => {
   const { data, loading, error, request } = useFetch()
 
   useEffect(() => {
     async function fetchPhotos() {
-      const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 })
-      const { res, json } = await request(url, options)
+      const { url, options } = PHOTOS_GET({ page: 1, total: 6, user })
+      const { json } = await request(url, options)
       console.log(json)
     }
     fetchPhotos()
-  }, [request])
+  }, [request, user])
 
   if (error) return <Error error={error} />
   if (loading) return <Loading />
